cc0107e96a6d311cff3732663d67000fb3266ab4
Sushobhan Agrawal <sa929@njit.edu>
2017-03-05 19:42:31 -0500

    code added to display category list and delete button

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -4,10 +4,23 @@
     <h1>Category List</h1>
     <table>
         <tr>
+            <th>Category_ID</th>
             <th>Name</th>
             <th>&nbsp;</th>
-        </tr>        
-        <!-- add category rows here -->
+        </tr>
+        
+        <?php foreach ($categories as $category) : ?>
+        <tr>
+          <td><?php echo $category['categoryID']; ?></td>
+          <td><?php echo $category['categoryName']; ?></td>
+          <td><form action="delete_category.php" method="post">
+              <input type="hidden" name="category_id"
+                      value="<?php echo $category['categoryID']; ?>">
+              <input type="submit" value="Delete">
+          </form></td>    
+        </tr>
+        
+    <?php endforeach; ?>    
     </table>
 
     <h2>Add Category</h2>
